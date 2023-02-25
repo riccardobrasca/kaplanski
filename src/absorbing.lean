e642a7aa1a86e6b82017b160e51443ed2c501023
Emilie Uthaiwat <emiliepathum@gmail.com>
2023-02-25 12:01:51 +0100

    finished section basic

--- src/absorbing.lean
+++ src/absorbing.lean
@@ -1,11 +1,11 @@
-import group_theory.subsemigroup.basic
+import group_theory.submonoid.basic
 import algebra.divisibility.basic
 import group_theory.submonoid.membership
 import algebra.associated
 
 namespace submonoid
 
-variables {M : Type*} [monoid M]
+variables {M : Type*} [comm_monoid M]
 
 def absorbing (S : submonoid M) : Prop :=
   ∀ x y, x * y ∈ S → ∃ z ∈ S, associated x z ∧ ∃ z ∈ S, associated y z
@@ -18,9 +18,11 @@ iff.rfl
 
 variable (M)
 
-lemma top_absorbing : (⊤ : submonoid M).absorbing := sorry
+lemma top_absorbing : (⊤ : submonoid M).absorbing := λ x y hxy, ⟨x, submonoid.mem_top _, associated.refl _, y, submonoid.mem_top _, associated.refl _⟩
 
-lemma bot_absorbing : (⊥ : submonoid M).absorbing := sorry
+lemma bot_absorbing : (⊥ : submonoid M).absorbing := λ x y hxy, ⟨1, (⊥ : submonoid M).one_mem, associated_one_of_mul_eq_one _ (submonoid.mem_bot.1 hxy), 1, (⊥ : submonoid M).one_mem, associated_one_of_mul_eq_one _ (submonoid.mem_bot.1 (by rwa mul_comm at hxy))⟩
+
+lemma is_unit.submonoid_absorbing : (is_unit.submonoid M).absorbing := λ x y hxy, ⟨x, is_unit_of_mul_is_unit_left hxy, associated.refl _, y, is_unit_of_mul_is_unit_right hxy, associated.refl _⟩
 
 end basic
 
