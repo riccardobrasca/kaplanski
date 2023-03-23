28f14b59cb4249e5cf922e45719bcf1dbac7617a
Emilie Uthaiwat <emiliepathum@gmail.com>
2023-03-23 22:17:06 +0100

    extracted lemma

--- src/localization.lean
+++ src/localization.lean
@@ -10,6 +10,14 @@ begin
   refine is_domain.to_cancel_comm_monoid_with_zero,
 end
 
+lemma is_localization.prime_of_prime {R S : Type*} [comm_ring R] [comm_ring S] [algebra R S] [is_domain R] {M : submonoid R} [is_localization M S] (hM : M ≤ non_zero_divisors R) (p : R) (hp : prime p) (hp₂ : ∃ (I : ideal R), disjoint (M : set R) (I : set R) ∧ p ∈ I) : prime ((algebra_map R S) p) :=
+begin
+  rcases hp₂ with ⟨I, hI, hp₂⟩,
+  have hp' := is_localization.is_prime_of_is_prime_disjoint M S (ideal.span {p}) ((ideal.span_singleton_prime hp.ne_zero).2 hp) (set.disjoint_of_subset_right (I.span_singleton_le_iff_mem.2 hp₂) hI),
+  rw [ideal.map_span, set.image_singleton] at hp',
+  exact (ideal.span_singleton_prime (λ hp₃, hp.ne_zero ((is_localization.to_map_eq_zero_iff S hM).1 hp₃))).1 hp',
+end
+
 example {R S : Type*} [comm_ring R] [comm_ring S] [algebra R S] [is_domain R] {M : submonoid R}
   [is_localization M S] (hM : M ≤ non_zero_divisors R) [unique_factorization_monoid R] :
   @unique_factorization_monoid S (@is_domain.to_cancel_comm_monoid_with_zero S _
@@ -25,13 +33,5 @@ begin
     rw [← is_localization.map_comap M S J, ← Idef, h, ideal.map_bot] },
   obtain ⟨p, hpI, hp⟩ := theo1_droite hI hIprime,
   refine ⟨algebra_map R S p, ideal.mem_comap.mp hpI, _⟩,
-  have hpzero : algebra_map R S p ≠ 0 :=
-    (map_ne_zero_iff (algebra_map R S) (is_localization.injective S hM)).2 hp.ne_zero,
-  rw [← ideal.span_singleton_prime hpzero, ← set.image_singleton, ← ideal.map_span],
-  refine is_localization.is_prime_of_is_prime_disjoint M S _
-    ((ideal.span_singleton_prime hp.ne_zero).2 hp) _,
-  refine set.disjoint_of_subset_right _
-    (((is_localization.is_prime_iff_is_prime_disjoint M S J).1 hJprime).2),
-  simp only [set_like.coe_subset_coe],
-  exact (ideal.span_singleton_le_iff_mem _).2 hpI,
+  refine is_localization.prime_of_prime hM p hp ⟨I, ⟨(((is_localization.is_prime_iff_is_prime_disjoint M S J).1 hJprime).2), hpI⟩⟩,
 end
